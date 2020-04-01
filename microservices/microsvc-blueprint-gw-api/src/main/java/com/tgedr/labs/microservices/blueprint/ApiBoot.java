1ff51ce0e307b2791d738b98b961ff26824881d3
joao tiago viegas <jtviegas.js@gmail.com>
2020-04-02 00:49:43 +0200

    finished config for ip throttling and related metrics

--- microservices/microsvc-blueprint-gw-api/src/main/java/com/tgedr/labs/microservices/blueprint/ApiBoot.java
+++ microservices/microsvc-blueprint-gw-api/src/main/java/com/tgedr/labs/microservices/blueprint/ApiBoot.java
@@ -2,8 +2,10 @@ package com.tgedr.labs.microservices.blueprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApiBoot {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBoot.class, args);
